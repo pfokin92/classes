8be9b10cb3646c0f9937d01ecfcd1051b8d74a61
Pavel Fokin <fokinpavel2007@outlook.com>
2021-08-27 00:01:44 +0300

    Second commit.

---
+++ src/js/typescharacter/character.js
@@ -0,0 +1,17 @@
+﻿export default class Character {
+  constructor(name, type, health, level, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+    if ((typeof (this.name) !== 'string') && this.name.length < 2 && this.name.length > 10) {
+      throw new Error('Ошибка. Имя не может быть таким ');
+    }
+    if (this.type !== 'Bowman' && this.type !== 'Swordsman' && this.type !== 'Magician' && this.type !== 'Daemon' && this.type !== 'Undead' && this.type !== 'Zombie') {
+      throw new Error('Ошибка. Не верно выбран тип');
+    }
+  }
+}
+
