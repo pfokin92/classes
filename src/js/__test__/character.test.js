6af9e10e143a8fcc04dadaeb5320f96abbfa00f7
Pavel Fokin <fokinpavel2007@outlook.com>
2021-09-03 23:58:47 +0300

    Third commit. Test name/type not working

--- src/js/__test__/character.test.js
+++ src/js/__test__/character.test.js
@@ -5,3 +5,16 @@ test('name', () => {
   const result = 'Ann';
   expect(newGamer.name).toBe(result);
 });
+
+test('name Error', () => {
+  const newGamer = new Character ('', 'Bowman');
+  const result = new Error ('Ошибка. Имя не может быть таким ');
+  expect(newGamer.name).toBe(result);
+})
+test('type Error', () => {
+  const newGamer = new Character ('Ann', 'Man');
+  const result = new Error ('Ошибка. Не верно выбран тип');
+  expect(newGamer.type).toBe(result);
+})
+
+
